29f733bf297d05b538ff252ec3c350737254e955
Romain Manni-Bucau <rmannibucau@apache.org>
2018-04-21 23:54:16 +0200

    no value lead to null and not emptySet

--- geronimo-jwt-auth-impl/src/main/java/org/apache/geronimo/microprofile/impl/jwtauth/jwt/GeronimoJsonWebToken.java
+++ geronimo-jwt-auth-impl/src/main/java/org/apache/geronimo/microprofile/impl/jwtauth/jwt/GeronimoJsonWebToken.java
@@ -16,7 +16,6 @@
  */
 package org.apache.geronimo.microprofile.impl.jwtauth.jwt;
 
-import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.toSet;
 
 import java.util.Set;
@@ -69,7 +68,7 @@ class GeronimoJsonWebToken implements JsonWebToken {
             if (claim.getType() == Set.class) {
                 final JsonValue jsonValue = delegate.get(claimName);
                 if (jsonValue == null) {
-                    return (T) emptySet();
+                    return null;
                 }
                 if (jsonValue.getValueType() == JsonValue.ValueType.ARRAY) {
                     return (T) JsonArray.class.cast(jsonValue).stream()
